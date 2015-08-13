fa6f8f20b88546ddc46859cff50d244b1a0a6c59
demoswebsites <demoswebsites@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-13 07:24:16 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/adsense-box/trunk@1219718 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- widget.php
+++ widget.php
@@ -29,7 +29,7 @@ class Adsense_Box_Widget extends WP_Widget {
 		
 		echo $args['after_widget'];
 		
-		echo '<!-- Adsense Box create at http://photoboxone.com -->';
+		echo '<!-- Adsense Box at http://photoboxone.com -->';
 		
 	}
 	
